68d7d36b97cd8607b478ecb1ff8afe816066cb00
hikari.kishimoto <hkr-sno-0112@outlook.jp>
2023-11-10 21:48:02 +0900

    APIClientの追加
    repositoryから直接APIを呼んでしまっていたため
    呼び出し処理はAPIClientから行うよう修正

---
+++ lib/api/search_resurt_api_client.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+/// githubAPI[Search repositories]へのAPIClient
+class SearchResultApiClient {
+  /// API呼び出しを行う
+  Future<Map<String, dynamic>> get(String searchWord) async {
+    final res = await http.get(Uri.parse('https://api.github.com/search/repositories?q=$searchWord'));
+    final statusCode = res.statusCode;
+    if (statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw Exception('[SearchGitHubRepository.fetchSearchResult] API STATUS ERROR statusCode = $statusCode');
+    }
+  }
+}
